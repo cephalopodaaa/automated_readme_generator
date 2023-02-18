c863a612af20d921754da5270bc48a8e165b9fe0
cephalopod <adamhannaford1@gmail.com>
2023-02-18 07:34:14 +0000

    collect info from user for readme

---
+++ index.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'choose your README title:',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Give a brief overview of the installation process',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Select a Licence for the project',
+            name: 'licence'
+        }
+    ])
+    .then((response) => {
+
+        console.log(response.title, response.description, response.installation, response.licence)
+
+
+    })
