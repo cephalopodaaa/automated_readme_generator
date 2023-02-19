de2ed6031d692f2e74c79c736129d7483d3fbedd
cephalopod <adamhannaford1@gmail.com>
2023-02-19 08:17:21 +0000

    made user options embed in template and started working on contents options to give user the option to have a contents or not

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-fs.readFile('template.md', 'utf8', (error, data) =>
-error ? console.error(error) : console.log(data)
-);
-
-
-
+// I wanted to import this template file and fill the spaces with 
+// fs.readFile('template.md', 'utf8', (error, data) =>
+//     error ? console.error(error) : let templateREADME = data
+// );
 
 inquirer
     .prompt([
@@ -17,14 +14,25 @@ inquirer
             name: 'title'
         },
         {
+            type: 'list',
+            message: 'Do you want a contents in your readme?',
+            name: 'contentsChoice',
+            choices: ['yes','no']
+        },
+        {
             type: 'input',
-            message: 'Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:',
+            message: 'Provide a short description explaining the what, why, and how of your project. What was the motivation? what problem does it solve? what did you learn?',
             name: 'description'
         },
         {
             type: 'input',
-            message: 'Give a brief overview of the installation process',
+            message: 'Give a brief What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
             name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use.',
+            name: 'usage'
         },
         {
             type: 'input',
@@ -33,8 +41,77 @@ inquirer
         }
     ])
     .then((response) => {
+        let makeContents;
+        response.contentsChoice === 'yes'
+            ? makeContents = true
+            : makeContents = false;
+            
+        console.log(makeContents)
+        
+        const README = `
+        
+\# ${response.title}
 
-        console.log(response.title, response.description, response.installation, response.licence)
+\#\# Description
 
+${response.description}
 
+\#\# Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+\#\# Installation
+
+${response.installation}
+
+\#\# Usage
+
+${response.usage}
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third - party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+${response.licence}
+The last section of a high - quality README file is the license.This lets other developers know what they can and cannot do with your project.If you need help choosing a license, refer to[https://choosealicense.com/](https://choosealicense.com/).
+
+        ---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document.You might also want to consider adding the following sections.
+
+## Badges
+
+        ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+            Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing.Check out the badges hosted by[shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.The[Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application.Then provide examples on how to run them here.
+
+        
+        `
+        console.log(README);
     })
+
+
+
--- template.md
+++ template.md
@@ -1,4 +1,5 @@
-# <Your-Project-Title>
+
+# ${response.title}
 
 ## Description
 
